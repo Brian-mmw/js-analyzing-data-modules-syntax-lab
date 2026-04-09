90cb2f4c6722727114e1904f4df381a5f1249572
brian <mwasbrayo73@gmail.commwasbrayo73@gmail.comclearsudo apt updategit config --global user.email mwasbrayo73@gmail.com>
2026-04-09 09:52:55 +0300

    Final submission: All tests passing

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const datejs = require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
+
+  // Gets today's date in M/d/yyyy format
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
